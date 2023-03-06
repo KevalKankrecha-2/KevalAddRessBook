3fc3b8e431ee513edca42147c42793cae487ebd1
kkj63609 <kkj63609@gmail.com>
2023-03-06 16:26:56 +0530

    Done Naming Contact

--- Areas/CON_Contact/Controllers/CON_ContactController.cs
+++ Areas/CON_Contact/Controllers/CON_ContactController.cs
@@ -23,11 +23,12 @@ namespace KevalThemeAddressBook.Areas.CON_Contact.Controllers
             Configuration = _configuration;
         }
         int UserID = 1;
-        CON_ContactModel modelcontact = new CON_ContactModel();
-        List<LOC_CountryDropDownModel> countrydropdown = new List<LOC_CountryDropDownModel>();
-        List<LOC_StateDropDown> statedropdown = new List<LOC_StateDropDown>();
-        List<LOC_CityDropDown> citydropdown = new List<LOC_CityDropDown>();
-        List<ContactCategoryDropDown> contactcategirydropdown = new List<ContactCategoryDropDown>();
+        CON_ContactModel modelCON_Contact = new CON_ContactModel();
+        List<LOC_CountryDropDownModel> CountryDropDownList = new List<LOC_CountryDropDownModel>();
+        List<LOC_StateDropDown> StateDropDownList = new List<LOC_StateDropDown>();
+        List<LOC_CityDropDown> CityDropDownList = new List<LOC_CityDropDown>();
+        List<ContactCategoryDropDown> ContactCategoryDropDownList = new List<ContactCategoryDropDown>();
+
 
         #region Open Contact Form
         public IActionResult OpenPage(int? ContactID)
@@ -36,110 +37,107 @@ namespace KevalThemeAddressBook.Areas.CON_Contact.Controllers
             string str = this.Configuration.GetConnectionString("myConnectionString");
 
             #region Get Country Drop Down And Pass it Where Form Open in Add/Edit Mode
-            LOC_DAL locdal = new LOC_DAL();
-            DataTable dt= locdal.LOC_Country_SelectForDropDown(str, UserID);
+            LOC_DAL dalLOC = new LOC_DAL();
+            DataTable dt= dalLOC.LOC_Country_SelectForDropDown(str, UserID);
             foreach (DataRow dr in dt.Rows)
             {
-                LOC_CountryDropDownModel dropdown = new LOC_CountryDropDownModel();
-                dropdown.CountryID = (int)dr["CountryID"];
-                dropdown.CountryName = (string)dr["CountryName"];
-                countrydropdown.Add(dropdown);
+                LOC_CountryDropDownModel CountryDropDown = new LOC_CountryDropDownModel();
+                CountryDropDown.CountryID = (int)dr["CountryID"];
+                CountryDropDown.CountryName = (string)dr["CountryName"];
+                CountryDropDownList.Add(CountryDropDown);
             }
-            ViewBag.CountryList = countrydropdown;
+            ViewBag.CountryList = CountryDropDownList;
             #endregion
 
             #region Get Data Of Contact Category Drop Down And Pass it Where Form Open in Add/Edit Mode
-            MST_DAL dalmst = new MST_DAL();
-            DataTable dtccddd = dalmst.ContactCategory_DropDownList(str, UserID);
+            MST_DAL dalMST = new MST_DAL();
+            DataTable dtccddd = dalMST.ContactCategory_DropDownList(str, UserID);
             foreach (DataRow dr in dtccddd.Rows)
             {
-                ContactCategoryDropDown dropdown = new ContactCategoryDropDown();
-                dropdown.ContactCaregoryID = (int)dr["ContactCategoryID"];
-                dropdown.ContactCategoryName = (string)dr["ContactCategoryName"];
-                contactcategirydropdown.Add(dropdown);
+                ContactCategoryDropDown ContactCategoryDropDown = new ContactCategoryDropDown();
+                ContactCategoryDropDown.ContactCaregoryID = (int)dr["ContactCategoryID"];
+                ContactCategoryDropDown.ContactCategoryName = (string)dr["ContactCategoryName"];
+                ContactCategoryDropDownList.Add(ContactCategoryDropDown);
             }
-            ViewBag.ContactCategoryDropDownList = contactcategirydropdown;
+            ViewBag.ContactCategoryDropDownList = ContactCategoryDropDownList;
             #endregion
 
             #region Contact Select By PK
             if (ContactID != null)
             {
-
                 string strcon = this.Configuration.GetConnectionString("myConnectionString");
-                CON_DAL condal = new CON_DAL();
-                DataTable dtupdt = condal.CON_Contact_SelectByPK(strcon,ContactID ,UserID);
+                CON_DAL dalCON = new CON_DAL();
+                DataTable dtupdt = dalCON.CON_Contact_SelectByPK(strcon,ContactID ,UserID);
                 foreach (DataRow drupt in dtupdt.Rows)
                 {
-                    modelcontact.ContactID = Convert.ToInt32(drupt["ContactID"]);
-                    modelcontact.ContactName = Convert.ToString(drupt["ContactName"]);
-                    modelcontact.ContactCategoryID = Convert.ToInt32(drupt["ContectCategoryID"]);
-                    modelcontact.ContactNo = Convert.ToString(drupt["ContactNo"]);
-                    modelcontact.WhatsappNo = Convert.ToString(drupt["WhatsappNo"]);
-                    modelcontact.Email = Convert.ToString(drupt["Email"]);
-                    modelcontact.Address = Convert.ToString(drupt["Address"]);
-                    modelcontact.FaceBookID = Convert.ToString(drupt["FaceBookID"]);
-                    modelcontact.InstaID = Convert.ToString(drupt["InstaID"]);
-                    modelcontact.CountryID = Convert.ToInt32(drupt["CountryID"]);
-                    modelcontact.StateID = Convert.ToInt32(drupt["StateID"]);
-                    modelcontact.CityID = Convert.ToInt32(drupt["CityID"]);
-                    modelcontact.Age = Convert.ToInt32(drupt["Age"]);
-                    modelcontact.BirthDate = Convert.ToDateTime(drupt["BirthDate"]);
-                    modelcontact.BloodGroup = Convert.ToString(drupt["BloodGroup"]);
+                    modelCON_Contact.ContactID = Convert.ToInt32(drupt["ContactID"]);
+                    modelCON_Contact.ContactName = Convert.ToString(drupt["ContactName"]);
+                    modelCON_Contact.ContactCategoryID = Convert.ToInt32(drupt["ContectCategoryID"]);
+                    modelCON_Contact.ContactNo = Convert.ToString(drupt["ContactNo"]);
+                    modelCON_Contact.WhatsappNo = Convert.ToString(drupt["WhatsappNo"]);
+                    modelCON_Contact.Email = Convert.ToString(drupt["Email"]);
+                    modelCON_Contact.Address = Convert.ToString(drupt["Address"]);
+                    modelCON_Contact.FaceBookID = Convert.ToString(drupt["FaceBookID"]);
+                    modelCON_Contact.InstaID = Convert.ToString(drupt["InstaID"]);
+                    modelCON_Contact.CountryID = Convert.ToInt32(drupt["CountryID"]);
+                    modelCON_Contact.StateID = Convert.ToInt32(drupt["StateID"]);
+                    modelCON_Contact.CityID = Convert.ToInt32(drupt["CityID"]);
+                    modelCON_Contact.Age = Convert.ToInt32(drupt["Age"]);
+                    modelCON_Contact.BirthDate = Convert.ToDateTime(drupt["BirthDate"]);
+                    modelCON_Contact.BloodGroup = Convert.ToString(drupt["BloodGroup"]);
                     ViewBag.EditImageURL= Convert.ToString(drupt["PhotoPath"]);
-                    modelcontact.PhotoPath= Convert.ToString(drupt["PhotoPath"]);
+                    modelCON_Contact.PhotoPath= Convert.ToString(drupt["PhotoPath"]);
                 }
 
                 #region Get State From Country
                 SqlConnection conn = new SqlConnection(strcon);
                 conn.Open();
                 SqlCommand cmd = conn.CreateCommand();
-                List<LOC_StateDropDown> stdropdown = new List<LOC_StateDropDown>();
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.CommandText = "PR_LOC_State_SelectStateDropDownByCountryID";
-                cmd.Parameters.AddWithValue("@CountryID", modelcontact.CountryID);
+                cmd.Parameters.AddWithValue("@CountryID", modelCON_Contact.CountryID);
                 cmd.Parameters.AddWithValue("@UserID", UserID);
                 SqlDataReader sdr =  cmd.ExecuteReader();
                 dt = new DataTable();
                 dt.Load(sdr);
                 foreach (DataRow dr in dt.Rows)
                 {
-                    LOC_StateDropDown dropdownstate = new LOC_StateDropDown();
-                    dropdownstate.StateID = Convert.ToInt32(dr["StateID"]);
-                    dropdownstate.StateName = Convert.ToString(dr["StateName"]);
-                    stdropdown.Add(dropdownstate);
+                    LOC_StateDropDown StateDropDown = new LOC_StateDropDown();
+                    StateDropDown.StateID = Convert.ToInt32(dr["StateID"]);
+                    StateDropDown.StateName = Convert.ToString(dr["StateName"]);
+                    StateDropDownList.Add(StateDropDown);
                 }
-                ViewBag.StateList = stdropdown;
-                #endregion
+                ViewBag.StateList = StateDropDownList;
+                #endregion 
 
                 # region Get City From State
-                List<LOC_CityDropDown> citydropdownlistedit = new List<LOC_CityDropDown>();
                 cmd = conn.CreateCommand();
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.CommandText = "PR_LOC_State_SelectCityDropDownByStateID";
                 cmd.Parameters.AddWithValue("@UserID", UserID);
-                cmd.Parameters.Add("@StateID", SqlDbType.Int).Value = modelcontact.StateID;
+                cmd.Parameters.Add("@StateID", SqlDbType.Int).Value = modelCON_Contact.StateID;
                 sdr = cmd.ExecuteReader();
                 dt = new DataTable();
                 dt.Load(sdr);
                 foreach (DataRow dr in dt.Rows)
                 {
-                    LOC_CityDropDown dropdowncity = new LOC_CityDropDown();
-                    dropdowncity.CityID = Convert.ToInt32(dr["CityID"]);
-                    dropdowncity.CityName = Convert.ToString(dr["CityName"]);
-                    citydropdownlistedit.Add(dropdowncity);
+                    LOC_CityDropDown CityDropDown = new LOC_CityDropDown();
+                    CityDropDown.CityID = Convert.ToInt32(dr["CityID"]);
+                    CityDropDown.CityName = Convert.ToString(dr["CityName"]);
+                    CityDropDownList.Add(CityDropDown);
                 }
-                ViewBag.CityList = citydropdownlistedit;
-                #endregion
+                ViewBag.CityList = CityDropDownList;
+                #endregion 
             }
             else
             {
-                List<LOC_StateDropDown> statedropdown = new List<LOC_StateDropDown>();
-                ViewBag.StateList = statedropdown;
-                List<LOC_CityDropDown> citydropdownlist = new List<LOC_CityDropDown>();
-                ViewBag.CityList = citydropdownlist;
+                List<LOC_StateDropDown> StateDropDown = new List<LOC_StateDropDown>();
+                ViewBag.StateList = StateDropDown;
+                List<LOC_CityDropDown> CityDropDown = new List<LOC_CityDropDown>();
+                ViewBag.CityList = CityDropDown;
             }
 
-            return View("CON_ContactAddEdit", modelcontact);
+            return View("CON_ContactAddEdit", modelCON_Contact);
         }
         #endregion
 
@@ -149,21 +147,21 @@ namespace KevalThemeAddressBook.Areas.CON_Contact.Controllers
         public IActionResult Index()
         {
             string strcon = this.Configuration.GetConnectionString("myConnectionString");
-            CON_DAL condal = new CON_DAL();
-            DataTable dt = condal.CON_Contact_SelectAll(strcon, UserID);
+            CON_DAL dalCON = new CON_DAL();
+            DataTable dt = dalCON.CON_Contact_SelectAll(strcon, UserID);
 
             /*To pass country drop down for filter in Contact list */
             SqlConnection conn = new SqlConnection(strcon);
-            LOC_DAL locdal = new LOC_DAL();
-            DataTable dt1 = locdal.LOC_Country_SelectForDropDown(strcon, UserID);
+            LOC_DAL dalLOC = new LOC_DAL();
+            DataTable dt1 = dalLOC.LOC_Country_SelectForDropDown(strcon, UserID);
             foreach (DataRow dr1 in dt1.Rows)
             {
-                LOC_CountryDropDownModel dropdown = new LOC_CountryDropDownModel();
-                dropdown.CountryID = (int)dr1["CountryID"];
-                dropdown.CountryName = (string)dr1["CountryName"];
-                countrydropdown.Add(dropdown);
+                LOC_CountryDropDownModel CountryDropDown = new LOC_CountryDropDownModel();
+                CountryDropDown.CountryID = (int)dr1["CountryID"];
+                CountryDropDown.CountryName = (string)dr1["CountryName"];
+                CountryDropDownList.Add(CountryDropDown);
             }
-            ViewBag.CountryList = countrydropdown;
+            ViewBag.CountryList = CountryDropDownList;
             /*end*/
             return View("CON_ContactList", dt);
         }
@@ -173,8 +171,8 @@ namespace KevalThemeAddressBook.Areas.CON_Contact.Controllers
         public IActionResult Delete(int ContactID)
         {
             string str = this.Configuration.GetConnectionString("myConnectionString");
-            CON_DAL condal = new CON_DAL();
-            condal.CON_Contact_DeleteByPK(str,ContactID, UserID);
+            CON_DAL dalCON = new CON_DAL();
+            dalCON.CON_Contact_DeleteByPK(str,ContactID, UserID);
             TempData["ContactMsg"] = "Contact Deleted successfully.!";
             return RedirectToAction("Index");
         }
@@ -202,14 +200,14 @@ namespace KevalThemeAddressBook.Areas.CON_Contact.Controllers
             string str = this.Configuration.GetConnectionString("myConnectionString");
             if (modelLOC_Contact.ContactID == null)
             {
-                CON_DAL condal = new CON_DAL();
-                String strmessage = condal.CON_Contact_Insert(str, UserID,modelLOC_Contact);
+                CON_DAL dalCON = new CON_DAL();
+                String strmessage = dalCON.CON_Contact_Insert(str, UserID,modelLOC_Contact);
                 TempData["ContactMsg"] = "Contact Inserted successfully.!";
             }
             else
             {
-                CON_DAL condal = new CON_DAL();
-                String strmessage = condal.CON_Contact_Update(str, UserID, modelLOC_Contact);
+                CON_DAL dalCON = new CON_DAL();
+                String strmessage = dalCON.CON_Contact_Update(str, UserID, modelLOC_Contact);
                 TempData["ContactMsg"] = "Contact Updated successfully.!";
             }
             return RedirectToAction("Index");
@@ -228,34 +226,32 @@ namespace KevalThemeAddressBook.Areas.CON_Contact.Controllers
         public IActionResult DropdownByCountryID(int CountryID)
         {
             string str = this.Configuration.GetConnectionString("myConnectionString");
-            LOC_DAL locdal = new LOC_DAL();
-            DataTable dt = locdal.dbo_PR_LOC_State_SelectDropDownByCountryID(str, CountryID, UserID);
-            List<LOC_StateDropDown> statedropdown = new List<LOC_StateDropDown>();
+            LOC_DAL dalLOC = new LOC_DAL();
+            DataTable dt = dalLOC.dbo_PR_LOC_State_SelectDropDownByCountryID(str, CountryID, UserID);
             foreach (DataRow dr in dt.Rows)
             {
                 LOC_StateDropDown dropdown = new LOC_StateDropDown();
                 dropdown.StateID = Convert.ToInt32(dr["StateID"]);
                 dropdown.StateName = dr["StateName"].ToString();
-                statedropdown.Add(dropdown);
+                StateDropDownList.Add(dropdown);
             }
-            var vModel = statedropdown;
+            var vModel = StateDropDownList;
             return Json(vModel);
         }
 
         public IActionResult DropdownByStateID(int StateID)
         {
             string str = this.Configuration.GetConnectionString("myConnectionString");
-            LOC_DAL locdal = new LOC_DAL();
-            DataTable dt=locdal.LOC_City_SelectDropDownByStateID(str,StateID,UserID);
-            List<LOC_CityDropDown> citydropdown = new List<LOC_CityDropDown>();
+            LOC_DAL dalLOC = new LOC_DAL();
+            DataTable dt=dalLOC.LOC_City_SelectDropDownByStateID(str,StateID,UserID);
             foreach (DataRow dr in dt.Rows)
             {
                 LOC_CityDropDown dropdown = new LOC_CityDropDown();
                 dropdown.CityID = Convert.ToInt32(dr["CityID"]);
                 dropdown.CityName = dr["CityName"].ToString();
-                citydropdown.Add(dropdown);
+                CityDropDownList.Add(dropdown);
             }
-            var vModel = citydropdown;
+            var vModel = CityDropDownList;
             return Json(vModel);
         }
         #endregion
@@ -264,21 +260,21 @@ namespace KevalThemeAddressBook.Areas.CON_Contact.Controllers
         public IActionResult Contact_Filter(int CountryID,int StateID,int CityID,string ContactName)
         {
             string str = this.Configuration.GetConnectionString("myConnectionString");
-            CON_DAL condal = new CON_DAL();
-            DataTable dt = condal.Contact_Filter(str,CountryID,StateID,CityID,ContactName,UserID);
+            CON_DAL dalCON = new CON_DAL();
+            DataTable dt = dalCON.Contact_Filter(str,CountryID,StateID,CityID,ContactName,UserID);
 
 
             /*To pass country drop down for filter in Contact list */
-            LOC_DAL locdal = new LOC_DAL();
-            DataTable dt1=locdal.LOC_Country_SelectForDropDown(str, UserID);
+            LOC_DAL dalLOC = new LOC_DAL();
+            DataTable dt1=dalLOC.LOC_Country_SelectForDropDown(str, UserID);
             foreach (DataRow dr1 in dt1.Rows)
             {
-                LOC_CountryDropDownModel dropdown = new LOC_CountryDropDownModel();
-                dropdown.CountryID = (int)dr1["CountryID"];
-                dropdown.CountryName = (string)dr1["CountryName"];
-                countrydropdown.Add(dropdown);
+                LOC_CountryDropDownModel CountryDropDown = new LOC_CountryDropDownModel();
+                CountryDropDown.CountryID = (int)dr1["CountryID"];
+                CountryDropDown.CountryName = (string)dr1["CountryName"];
+                CountryDropDownList.Add(CountryDropDown);
             }
-            ViewBag.CountryList = countrydropdown;
+            ViewBag.CountryList = CountryDropDownList;
             /*end*/
 
             return View("CON_ContactList",dt);
